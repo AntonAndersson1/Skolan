232415d0aedc099465a7cedf5f2e9434b5f15e95
AntonAndersson1 <anton.andersson5@elev.ga.ntig.se>
2023-12-21 12:06:52 +0100

    Byta äåö
    jag bytte alla variabel namn med aåö till a eller o

--- skolan.py
+++ skolan.py
@@ -21,23 +21,23 @@ Dead = ("""
 
 
 
-def karaktär():
-    karaktärmeny = input("Vad heter du?")
+def karaktar():
+    karaktarmeny = input("Vad heter du?")
     print(f"Väldigt bra namn valt av din mamma och pappa!")
 
-def Insällningar(svenska, engelska, swedish, english):
-    Inställningar = input("Vill du ha spelet på svenska eller engelska, Do you want the game in swedish or english")
+def Insallningar(svenska, engelska, swedish, english):
+    Installningar = input("Vill du ha spelet på svenska eller engelska, Do you want the game in swedish or english")
     if svenska == True or swedish == True:
         print ("Spelet kommer vara på svenska")
         if engelska == True or english == True:
             print("The game will be on English")
 
 def Spel():
-    vald_dörr = int(input("Vilken dörr vill du gå igenom (1, 2 eller 3)? "))
+    vald_dorr = int(input("Vilken dörr vill du gå igenom (1, 2 eller 3)? "))
 
-    farlig_dörr = random.randint(1, 3)
+    farlig_dorr = random.randint(1, 3)
 
-    if vald_dörr == farlig_dörr:
+    if vald_dorr == farlig_dorr:
         print("du överlever och kommer till nästa par var dörrar.")
     else:
         print(f"{Dead}")
@@ -50,9 +50,9 @@ while True:
         """
 1. Starta Spelet
 
-2. Karaktär Meny
+2. Karaktar Meny
 
-3. Inställningar
+3. Installningar
 
 4. Avaluta Spelet
         """
@@ -61,8 +61,8 @@ while True:
     if menyval == "1":
         Spel()
     if menyval == "2":
-        karaktär()
+        karaktar()
     if menyval == "3":
-        Insällningar()
+        Insallningar()
     if menyval == "4":
         break
